4b1c0422c154c0eac2da8482fb6bb54010e81da8
Yordan Milkov <milkov85@gmail.com>
2020-10-15 12:03:24 +0300

    New cahnges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  hideDetails = true;
+  detailsClicked = [];
+
+  details() {
+    let i = this.detailsClicked.length + 1;
+    this.detailsClicked.push(i);
+    return this.hideDetails = !this.hideDetails;
+  }
+
+  changeColor() {
+    return this.detailsClicked.length < 5 ?"black":"blue"
+  }
 }
